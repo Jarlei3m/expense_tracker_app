8969e8418288c2548fbdd157ad4dc3689e1a788e
Járlei Rodrigues <jarlei_rodrigues@outlook.com>
2023-06-14 21:35:58 -0300

    feat: added mocked expenses data

--- components/ExpensesOutput/ExpensesOutput.js
+++ components/ExpensesOutput/ExpensesOutput.js
@@ -3,10 +3,43 @@ import { View } from "react-native";
 import ExpensesSummary from "./ExpensesList";
 import ExpensesList from "./ExpensesList";
 
-function ExpensesOutput({ expenses }) {
+const DUMMY_EXPENSES = [
+  {
+    id: "e1",
+    description: "A pair of shoes",
+    amount: 59.99,
+    date: new Date("2021-12-19"),
+  },
+  {
+    id: "e2",
+    description: "A pair of trousers",
+    amount: 89.29,
+    date: new Date("2022-01-05"),
+  },
+  {
+    id: "e3",
+    description: "A T-shirt",
+    amount: 45.99,
+    date: new Date("2021-12-01"),
+  },
+  {
+    id: "e4",
+    description: "A book",
+    amount: 14.99,
+    date: new Date("2021-02-19"),
+  },
+  {
+    id: "e4",
+    description: "Another book",
+    amount: 18.59,
+    date: new Date("2023-01-11"),
+  },
+];
+
+function ExpensesOutput({ expenses, expensesPeriod }) {
   return (
     <View>
-      <ExpensesSummary />
+      <ExpensesSummary expenses={expenses} periodName={expensesPeriod} />
       <ExpensesList />
     </View>
   );
