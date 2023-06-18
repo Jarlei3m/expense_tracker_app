16e566c35d55817760738d57905f3be6567d8141
Járlei Rodrigues <jarlei_rodrigues@outlook.com>
2023-06-18 08:37:31 -0300

    feat: custom input component

---
+++ components/ManageExpense.js/ExpenseForm.js
@@ -0,0 +1,13 @@
+import { StyleSheet, TextInput, View } from "react-native";
+
+function ExpenseForm() {
+  return (
+    <View>
+      <TextInput />
+    </View>
+  );
+}
+
+export default ExpenseForm;
+
+const styles = StyleSheet.create({});
